5931c9a51afc2722904e8e5fafc43eb359fa2dc2
sifen <sifenbiyadegelegn@gmail.com>
2026-01-01 10:44:34 +0300

    add cors for frontend

--- diabetes_api.py
+++ diabetes_api.py
@@ -2,11 +2,30 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://ml-fastapi-backend-diabetes.onrender.com"
+]
+
+
+app.add_middleware(
+
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 log_reg = None
 dt_model = None
+
 
 class DiabetesFeatures(BaseModel):
     Pregnancies: float
